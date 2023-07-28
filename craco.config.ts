e407f0c0802bf14f4276f0ec7b4897a1483f556b
moroz-art-dev <moroz.art.dev@gmail.com>
2023-07-28 15:43:20 +0300

    basic config craco

---
+++ craco.config.ts
@@ -0,0 +1,19 @@
+export {};
+//eslint-disable-next-line @typescript-eslint/no-var-requires
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {},
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
